6feb978d234002515d63b013916b729eb0c9817c
KevinChiguano <kevin_chili1998@hotmail.com>
2022-08-29 20:25:50 -0500

    Stream's y programacion en paralelo

--- src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
+++ src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
@@ -1,11 +1,31 @@
 package com.uce.edu.demo.service.funcional;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.jboss.logging.Logger;
 
 public class MainInterfacesFuncionales {
 	
 	private static final Logger LOGGER = Logger.getLogger(MainInterfacesFuncionales.class);
 
+	public static boolean prueba(Integer numero) {
+		return numero >=3;
+	}
+	
+	public static void imprimir(String cadena) {
+		LOGGER.info("Impresion: "+cadena);
+	}
+	
+	public static void guardar(String elementoAInsertar) {
+		//Consumo el metodo 
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -37,6 +57,11 @@ public class MainInterfacesFuncionales {
 			});
 		LOGGER.info("HO Supplier "+ valorHO1);
 		
+		//JAVA
+		LOGGER.info("JAVA Supplier ");
+		Stream<String> test = Stream.generate(() -> "Edison 3").limit(7);
+		test.forEach(cadena -> System.out.println(cadena));
+		
 		//CONSUMER
 		
 		//clases
@@ -50,6 +75,10 @@ public class MainInterfacesFuncionales {
 		//Metodos High Order
 		metodosHO.consumirConsumer(valor -> System.out.println(valor), 2);
 		
+		//JAVA
+		LOGGER.info("JAVA Consumer");
+		List<Integer> listaNumeros = Arrays.asList(1,2,3,4,5);
+		listaNumeros.forEach(numero -> System.out.println(numero));
 		
 		//PREDICATE
 		
@@ -61,6 +90,11 @@ public class MainInterfacesFuncionales {
 		//Metodos High Order
 		boolean respuesta = metodosHO.consumirPredicate(cadena -> cadena.contains("A"), "Kevin con Z");
 		LOGGER.info("HO Predicate "+respuesta);
+		
+		//JAVA
+		LOGGER.info("JAVA Predicate");
+		Stream<Integer> nuevaLista = listaNumeros.stream().filter(valor -> prueba(valor));
+		nuevaLista.forEach(numero -> System.out.println(numero));
 		
 		//FUNCTION
 		
@@ -74,6 +108,9 @@ public class MainInterfacesFuncionales {
 		
 		LOGGER.info("Function Lambdas: "+ functionLambda.aplicar("7"));
 		
+		//JAVA
+		
+		
 		//Metodos High Order
 		String valorFinalHO = metodosHO.consumirFunction(valor -> {
 			String retorn = valor.toString()+"A";
@@ -81,6 +118,25 @@ public class MainInterfacesFuncionales {
 		}, 1);
 		
 		LOGGER.info("HO Function: "+valorFinalHO);
+		
+		LOGGER.info("JAVA Function");
+		//Conversiones/cast Empleado EmpleadoDTO (Ligero)
+		Stream<String> listaCambiada = listaNumeros.stream().map(numeroLista -> {
+			Integer valor = numeroLista+1;
+			String cadena = "Num: "+valor.toString();
+			return cadena;
+		});
+		
+		//Declarativa: ideas/inteciones
+		listaCambiada.forEach(valor -> imprimir(valor));
+		
+		List<String> lista1 = new ArrayList<>();
+		Map<String, String> mp1 = new HashMap<String, String>();
+		
+		//Imperativa: paso a paso
+		//for(Integer valor:) {
+		//	
+		//}
 		
 		//UNARY OPERATOR (FUNCTION)
 		
@@ -93,6 +149,9 @@ public class MainInterfacesFuncionales {
 		};
 		
 		LOGGER.info("Unari Operator Lamda: "+ unaryLambda.apply("Daniel"));
+		
+		//JAVA
+		
 	}
 
 }
